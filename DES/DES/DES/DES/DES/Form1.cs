c8277847ae677566ae8b9dd7cc30a8e9ba10c9ce
BetkoksPI19D <65999327+BetkoksPI19D@users.noreply.github.com>
2021-03-10 16:20:06 +0200

    Second commit

---
+++ DES/DES/DES/DES/DES/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace DES
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static string Encrypt(string pword, string key)
+        {
+            byte[] pwordBytes = ASCIIEncoding.ASCII.GetBytes(pword);
+            byte[] keyBytes = ASCIIEncoding.ASCII.GetBytes(key);
+
+            DESCryptoServiceProvider provider = new DESCryptoServiceProvider();
+            ICryptoTransform transform = provider.CreateEncryptor(pwordBytes, keyBytes);
+            CryptoStreamMode mode = CryptoStreamMode.Write;
+
+            MemoryStream memStream = new MemoryStream();
+            CryptoStream cryptoStream = new CryptoStream(memStream, transform, mode);
+            cryptoStream.Write(pwordBytes, 0, pwordBytes.Length);
+            cryptoStream.FlushFinalBlock();
+
+            byte[] encryptedWordBytes = new byte[memStream.Length];
+            memStream.Position = 0;
+            memStream.Read(encryptedWordBytes, 0, encryptedWordBytes.Length);
+
+            string encryptedWord = Convert.ToBase64String(encryptedWordBytes);
+
+            return encryptedWord;
+        }
+    }
+}
---
+++ DES/DES/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace DES
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+       
+        
+        //static byte[] DESCrypto(Mode mode, byte[] IV, byte[] key, byte[] word)
+        //{
+        //    using (var DES = new DESCryptoServiceProvider())
+        //    {
+        //        DES.IV = IV;
+        //        DES.Mode = CipherMode.ECB;
+        //        DES.Padding = PaddingMode.PKCS7;
+
+        //            using (var memStream = new MemoryStream())
+        //            {
+        //                CryptoStream cryptoStream = null;
+
+        //                if (mode == Mode.Ecrypt)
+        //                    cryptoStream = new CryptoStream(memStream, DES.CreateEncryptor(), CryptoStreamMode.Write);
+        //                else if (mode == Mode.Decrypt)
+        //                    cryptoStream = new CryptoStream(memStream, DES.CreateDecryptor(), CryptoStreamMode.Write);
+        //                if (cryptoStream == null)
+        //                    return null;
+
+        //            cryptoStream.Write(word, 0, word.Length);
+        //            cryptoStream.FlushFinalBlock();
+        //            return memStream.ToArray();
+
+        //            }
+        //        return null;
+        //    }
+        //}
+        //static byte[] Decryption(byte[] IV, byte[] key, byte[] word)
+        //{
+        //    return null;
+        //}
+
+        public string Encryptd(string strWord, string strKey, bool ECB)
+        {
+            byte[] key = new byte[8];
+            byte[] byteArray;
+            byte[] hash;
+
+            try
+            {
+                MD5CryptoServiceProvider hsh = new MD5CryptoServiceProvider();
+                hash = hsh.ComputeHash(Encoding.ASCII.GetBytes(strKey));
+                for (int i = 0; i < 8; i++)
+                    key[i] = hash[i];
+                DESCryptoServiceProvider DES = new DESCryptoServiceProvider();
+                if (ECB == true)
+                {
+                    DES.Mode = CipherMode.ECB;
+                    DES.Key = key;
+                }
+                else
+                {
+                    DES.Mode = CipherMode.CBC;
+                    DES.Key = key;
+                    DES.GenerateIV();
+                }
+                byteArray = Encoding.ASCII.GetBytes(strWord);
+                MemoryStream memStream = new MemoryStream();
+                CryptoStream cryStream = new CryptoStream(memStream, DES.CreateEncryptor(), CryptoStreamMode.Write);
+                cryStream.Write(byteArray, 0, byteArray.Length);
+                cryStream.FlushFinalBlock();
+
+                return Convert.ToBase64String(memStream.ToArray());
+
+            }
+            catch (Exception exc)
+            {
+                throw exc;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
